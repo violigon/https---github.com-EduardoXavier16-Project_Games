195ba49325c10228e21a8793732bc58f322e9e50
EduardoXavier16 <89054003+EduardoXavier16@users.noreply.github.com>
2022-05-24 11:17:39 -0300

    Update of code

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/createUser.Dto';
 import { User } from './entities/user.Entity.Dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
   users: User[] = [];
+  constructor(private prisma: PrismaService) {}
   findAll(): User[] {
     return this.users;
   }
